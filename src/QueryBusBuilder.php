e3b582e7327f00bc79e97038d55d7c0bd3b8aa69
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2021-12-08 16:02:48 +0100

    Initial setup

---
+++ src/QueryBusBuilder.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 08/12/2021
+ */
+
+declare(strict_types=1);
+
+namespace JeckelLab\QueryDispatcher;
+
+class QueryBusBuilder
+{
+
+}
---
+++ tests/QueryBusBuilderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 08/12/2021
+ */
+
+namespace Tests\JeckelLab\QueryDispatcher;
+
+use JeckelLab\QueryDispatcher\QueryBusBuilder;
+use PHPUnit\Framework\TestCase;
+
+class QueryBusBuilderTest extends TestCase
+{
+    public function testBuilder(): void
+    {
+        $this->assertTrue(true);
+    }
+}
